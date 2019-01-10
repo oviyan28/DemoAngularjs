a61d99cf5a233881eafb4a9df51856089fbb90a1
Dell <Dell@192.168.1.17>
2019-01-10 12:56:02 +0530

    Demo

---
+++ demoangularjs2/WebContent/NameController.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+app.Controller('NameController',function($scope){
+$scope.firstname="John"
+$scope.lastname="Smith"
+alert($scope.firstname + " " +$scope.lastname)
+//	$scope.UpdatedNames=function(){
+//	alert($scope.firstname + " " + $scope.lastname)
+	//}
+})
