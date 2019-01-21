af131089450b4a72562d3c86ab15389ff8558529
Dell <Dell@192.168.1.17>
2019-01-21 11:23:41 +0530

    demoangularjs

--- demoangularajs/WebContent/RouteConfig.js
+++ demoangularajs/WebContent/RouteConfig.js
@@ -5,6 +5,6 @@ var myApp=angular.module("myApp",['ngRoute']);
 myApp.config(function($routeProvider)
 {
 alert("This is Route Provider");
-	$routeProvider.when("/login",{templateUrl:"MyFolder/Login.html"})
-	    .when("/register",{templateUrl:"MyFolder/Register.html"});
+	$routeProvider.when("/login",{templateUrl:"Login.html"})
+	    .when("/register",{templateUrl:"Register.html"});
 });
